573deac9f1c230593a2a93e6bdc2bfb07cd3b943
Langston Kriel <langston.kriel@bytecode.io>
2022-10-29 04:53:12 +0000

    array build adj

--- customviz.js
+++ customviz.js
@@ -47,7 +47,7 @@ const visObject = {
         prev[val[iterator].value] = []
         iterator_list.push(val[iterator].value)
       }
-      val.shift;
+      val.shift();
       prev[val[iterator].value].push(val)
       return prev
       },{});
